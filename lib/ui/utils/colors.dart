c8ef9e5ed4b953255d9bd45daed1f3c8351b0349
amir mojarad <amirmojarad13@gmail.com>
2021-05-11 12:22:40 +0430

    change colors

--- lib/ui/utils/colors.dart
+++ lib/ui/utils/colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-const Color background = Color(0xfff2fee8);
-const Color accent = Color(0xff34b300);
+const Color background = Color(0xffFefefe);
+const Color accent = Color(0xff643b9f);
 const Color divider = Color(0xff3c3c3c);
 const Color textBlack = Color(0xff1c1c1c);
 const Color textWhite = Color(0xfff2f2f2);
 const Color delete = Color(0xffb30303);
-const Color button = Color(0xff008f00);
-const Color activeBorder = Color(0xff72d03f);
-const Color deActiveBorder = Color(0xffd1efbf);
+const Color button = Color(0xff371f76);
+const Color activeBorder = Color(0xff643b9f);
+const Color deActiveBorder = Color(0xff806A9F);
 const Color shadowText = Color(0xffadadad);
 const Color wordCard = Color(0xffb1e494);
